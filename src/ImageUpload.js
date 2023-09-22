01956363a4fd5abf8bdeb6949692a5037f67f71f
Alex Wu <alexwu606@gmail.com>
2023-09-21 22:02:42 -0400

    href open new tab instead

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -132,7 +132,7 @@ function ImageUpload(props) {
             {selectedImage && <TextField onBlur={handlePixelSizeChange} type="number" id="pixelSize" label="Pixel Size (Performance Issues)" variant="filled" defaultValue={pixelSize} />}
             <div className="uploadedImage">
                 <div>
-                    {!!sourceUrl && <div>Source: <a href={sourceUrl}>{sourceUrl}</a></div>}
+                    {!!sourceUrl && <div>Source: <a target="_blank" rel="noopener noreferrer" href={sourceUrl}>{sourceUrl}</a></div>}
                     {isImageLoading && <CircularProgress />}
                 </div>
                 {didImageFailToLoad && <div className="errorScreen">
